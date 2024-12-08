06dce45011f8803fccaca34b6195578bbafe35ea
Tomas229 <tomas229cortez@gmail.com>
2024-12-08 10:43:03 -0300

    feat(2024-8): Agrega archivos parte 1

---
+++ challenges/2024/08/solution.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Year2024
+  # Solution
+  class Day08 < Solution
+    # @input is available if you need the raw data input
+    # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
+
+    def part_1
+      nil
+    end
+
+    def part_2
+      nil
+    end
+  end
+end

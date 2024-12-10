ea4eab371b401fd43a5094a8d28fb38213d0f33a
Tomas229 <tomas229cortez@gmail.com>
2024-12-09 23:52:36 -0300

    feat(2024-9): Agrega respuesta parte 2

--- challenges/2024/09/solution.rb
+++ challenges/2024/09/solution.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Year2024
-  # SOlution
+  # Solution
   class Day09 < Solution
     # @input is available if you need the raw data input
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
@@ -20,7 +20,15 @@ module Year2024
     end
 
     def part_2
-      nil
+      array = parse_filesystem_2
+
+      reverse_index = array.size
+      while (reverse_index -= 1).positive?
+        next unless (item = array[reverse_index])[:type] == :data
+
+        reverse_index += search_memory(array, reverse_index, item)
+      end
+      check_sum_2(array)
     end
 
     # Processes each line of the input file and stores the result in the dataset
@@ -42,6 +50,17 @@ module Year2024
       sum
     end
 
+    def check_sum_2(array)
+      sum = 0
+      index = -1
+      array.each do |c|
+        new_index = index + c[:quantity]
+        sum += c[:data_index] * ((((new_index + 1) * new_index) / 2) - (((index + 1) * index) / 2)) if c[:type] == :data
+        index = new_index
+      end
+      sum
+    end
+
     def parse_filesystem
       array = []
       data.chars.each_with_index do |num, index|
@@ -52,6 +71,42 @@ module Year2024
                  end
       end
       array
+    end
+
+    def parse_filesystem_2
+      array = []
+      data.chars.each_with_index do |num, index|
+        array += if index.even?
+                   [{ type: :data, quantity: num.to_i, data_index: index / 2 }]
+                 else
+                   [{ type: :space, quantity: num.to_i }]
+                 end
+      end
+      array
+    end
+
+    def search_memory(array, reverse_index, item)
+      array.each_with_index do |space, i|
+        break if i > reverse_index
+        next unless space[:type] == :space && (delta = space[:quantity] - item[:quantity]) > -1
+
+        move_file(array, delta, i, reverse_index)
+        return delta.positive? ? 1 : 0
+      end
+      0
+    end
+
+    def move_file(array, delta, space_index, file_index)
+      space = array[space_index]
+      file = array[file_index]
+      if delta.positive?
+        space[:quantity] = delta
+        array[file_index] = { type: :space, quantity: file[:quantity] }
+        array.insert(space_index, file)
+      else
+        array[space_index] = file
+        array[file_index] = space
+      end
     end
   end
 end
--- spec/2024/09/solution_spec.rb
+++ spec/2024/09/solution_spec.rb
@@ -21,12 +21,12 @@ RSpec.describe Year2024::Day09 do
   end
 
   describe 'part 2' do
-    it 'returns nil for the example input' do
-      expect(described_class.part_2(example_input)).to eq(nil)
+    it 'returns 2858 for the example input' do
+      expect(described_class.part_2(example_input)).to eq(2858)
     end
 
-    it 'returns nil for my input' do
-      expect(described_class.part_2(input)).to eq(nil)
+    it 'returns 6307653242596 for my input' do
+      expect(described_class.part_2(input)).to eq(6_307_653_242_596)
     end
   end
 end

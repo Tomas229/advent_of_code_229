ae5c05f99926ea4ed7f0adbdb17be55ee98c2d91
Tomas229 <tomas229cortez@gmail.com>
2024-12-11 00:17:05 -0300

    feat(2024-10): Agrega respuesta parte 1

--- challenges/2024/10/solution.rb
+++ challenges/2024/10/solution.rb
@@ -5,13 +5,36 @@ module Year2024
   class Day10 < Solution
     # @input is available if you need the raw data input
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
+    COORDS = [[0, - 1], [0, 1], [- 1, 0], [1, 0]].freeze
 
     def part_1
-      nil
+      sum = 0
+      @grid.each_coord do |x, y|
+        sum += find_peak(x, y).uniq.size if @grid.coord(x, y).zero?
+      end
+      sum
     end
 
     def part_2
       nil
+    end
+
+    private
+
+    def setup_data
+      @grid = Grid.new(data.map(&:chars).map { |a| a.map(&:to_i) })
+    end
+
+    def find_peak(x_coords, y_coords)
+      value = @grid.coord(x_coords, y_coords)
+      return [[x_coords, y_coords]] if value == 9
+
+      res = []
+      COORDS.each do |coord|
+        new_cords = coord.zip([x_coords, y_coords]).map(&:sum)
+        res += find_peak(*new_cords) if @grid.coord(*new_cords)&.public_send(:-, value) == 1
+      end
+      res
     end
   end
 end
--- spec/2024/10/solution_spec.rb
+++ spec/2024/10/solution_spec.rb
@@ -6,18 +6,24 @@ RSpec.describe Year2024::Day10 do
   let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2024/10/input.txt')) }
   let(:example_input) do
     <<~EOF
-      change
-      me
+      89010123
+      78121874
+      87430965
+      96549874
+      45678903
+      32019012
+      01329801
+      10456732
     EOF
   end
 
   describe 'part 1' do
-    it 'returns nil for the example input' do
-      expect(described_class.part_1(example_input)).to eq(nil)
+    it 'returns 36 for the example input' do
+      expect(described_class.part_1(example_input)).to eq(36)
     end
 
-    it 'returns nil for my input' do
-      expect(described_class.part_1(input)).to eq(nil)
+    it 'returns 548 for my input' do
+      expect(described_class.part_1(input)).to eq(548)
     end
   end
 

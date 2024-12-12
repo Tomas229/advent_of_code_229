9f991a336f0b219ee668c9d935d4320c69f2d127
Tomas229 <tomas229cortez@gmail.com>
2024-12-12 01:13:32 -0300

    feat(2024-11): Agrega respuesta parte 1

--- challenges/2024/11/solution.rb
+++ challenges/2024/11/solution.rb
@@ -7,11 +7,42 @@ module Year2024
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
 
     def part_1
-      nil
+      sum = 0
+      data.split.each do |n|
+        sum += stone_division(n, 25).size
+      end
+      sum
     end
 
     def part_2
       nil
+    end
+
+    private
+
+    def stone_division(number, blinks)
+      return [number] if blinks < 1
+
+      if number == '0'
+        divide_zero_rock(blinks)
+      elsif number.size.even?
+        divide_even_rock(number, blinks)
+      else
+        divide_odd_rock(number, blinks)
+      end
+    end
+
+    def divide_zero_rock(blinks)
+      stone_division('1', blinks - 1)
+    end
+
+    def divide_even_rock(number, blinks)
+      half = number.size / 2
+      stone_division(number[...half].to_i.to_s, blinks - 1) + stone_division(number[half..].to_i.to_s, blinks - 1)
+    end
+
+    def divide_odd_rock(number, blinks)
+      stone_division((number.to_i * 2024).to_s, blinks - 1)
     end
   end
 end
--- spec/2024/11/solution_spec.rb
+++ spec/2024/11/solution_spec.rb
@@ -6,18 +6,17 @@ RSpec.describe Year2024::Day11 do
   let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2024/11/input.txt')) }
   let(:example_input) do
     <<~EOF
-      change
-      me
+      125 17
     EOF
   end
 
   describe 'part 1' do
-    it 'returns nil for the example input' do
-      expect(described_class.part_1(example_input)).to eq(nil)
+    it 'returns 55312 for the example input' do
+      expect(described_class.part_1(example_input)).to eq(55_312)
     end
 
-    it 'returns nil for my input' do
-      expect(described_class.part_1(input)).to eq(nil)
+    it 'returns 189547 for my input' do
+      expect(described_class.part_1(input)).to eq(189_547)
     end
   end
 

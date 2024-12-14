e43e868277114264f30663bce6d3cab85aae14a7
Tomas229 <tomas229cortez@gmail.com>
2024-12-14 17:30:18 -0300

    feat(2024-13): Agrega respuesta parte 1

--- challenges/2024/13/solution.rb
+++ challenges/2024/13/solution.rb
@@ -8,11 +8,42 @@ module Year2024
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
 
     def part_1
-      nil
+      tokens = 0
+      index = 0
+      while index < data.size
+        tokens += token_calculator(data, index)
+        index += 4
+      end
+      tokens
     end
 
     def part_2
       nil
+    end
+
+    private
+
+    def token_calculator(data, index)
+      coefficients = coefficients_matrix_creator(data, index)
+      return 0 if coefficients.determinant.zero?
+
+      res = sol_calculator(data, index, coefficients)
+      a = res.element(0, 0)
+      b = res.element(1, 0)
+      return 0 if a.denominator != 1 && b.denominator != 1
+
+      (3 * a.to_i) + b.to_i
+    end
+
+    def coefficients_matrix_creator(data, index)
+      eq_1 = data[index].scan(/\d+/)
+      eq_2 = data[index + 1].scan(/\d+/)
+      Matrix[[eq_1[0].to_i, eq_2[0].to_i], [eq_1[1].to_i, eq_2[1].to_i]]
+    end
+
+    def sol_calculator(data, index, coefficients)
+      eq_3 = data[index + 2].scan(/\d+/)
+      coefficients.inverse * Matrix[[eq_3[0].to_i], [eq_3[1].to_i]]
     end
   end
 end
--- spec/2024/13/solution_spec.rb
+++ spec/2024/13/solution_spec.rb
@@ -6,18 +6,31 @@ RSpec.describe Year2024::Day13 do
   let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2024/13/input.txt')) }
   let(:example_input) do
     <<~EOF
-      change
-      me
+      Button A: X+94, Y+34
+      Button B: X+22, Y+67
+      Prize: X=8400, Y=5400
+
+      Button A: X+26, Y+66
+      Button B: X+67, Y+21
+      Prize: X=12748, Y=12176
+
+      Button A: X+17, Y+86
+      Button B: X+84, Y+37
+      Prize: X=7870, Y=6450
+
+      Button A: X+69, Y+23
+      Button B: X+27, Y+71
+      Prize: X=18641, Y=10279
     EOF
   end
 
   describe 'part 1' do
-    it 'returns nil for the example input' do
-      expect(described_class.part_1(example_input)).to eq(nil)
+    it 'returns 480 for the example input' do
+      expect(described_class.part_1(example_input)).to eq(480)
     end
 
-    it 'returns nil for my input' do
-      expect(described_class.part_1(input)).to eq(nil)
+    it 'returns 29388 for my input' do
+      expect(described_class.part_1(input)).to eq(29_388)
     end
   end
 

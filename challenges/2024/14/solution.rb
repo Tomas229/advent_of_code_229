84469d601ba1768650ddc0d4967f46aad2ce16a0
Tomas229 <tomas229cortez@gmail.com>
2024-12-15 12:30:30 -0300

    feat(2024-14): Agrega respuesta parte 1

--- challenges/2024/14/solution.rb
+++ challenges/2024/14/solution.rb
@@ -7,21 +7,41 @@ module Year2024
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
 
     def part_1
-      nil
+      @x = data.size > 20 ? 101 : 11
+      @y = data.size > 20 ? 103 : 7
+      @quadrants = { '1': 0, '2': 0, '3': 0, '4': 0 }
+      data.each do |line|
+        take_step(line)
+      end
+      @quadrants.values.reduce(:*)
     end
 
     def part_2
       nil
     end
 
-    # Processes each line of the input file and stores the result in the dataset
-    # def process_input(line)
-    #   line.map(&:to_i)
-    # end
+    private
 
-    # Processes the dataset as a whole
-    # def process_dataset(set)
-    #   set
-    # end
+    def take_step(line)
+      x, y = obtain_position(line)
+      x_bound = @x / 2
+      y_bound = @y / 2
+      update_quadrant_count(x, y, x_bound, y_bound) if x != x_bound && y != y_bound
+      [x, y]
+    end
+
+    def obtain_position(line)
+      nums = line.scan(/-?\d+/).map(&:to_i)
+      [(nums[0] + (nums[2] * 100)) % @x, (nums[1] + (nums[3] * 100)) % @y]
+    end
+
+    def update_quadrant_count(x_coord, y_coord, x_bound, y_bound)
+      quadrant = if x_coord < x_bound
+                   y_coord < y_bound ? :'1' : :'4'
+                 elsif x_coord > x_bound
+                   y_coord < y_bound ? :'2' : :'3'
+                 end
+      @quadrants[quadrant] += 1 if quadrant
+    end
   end
 end
--- spec/2024/14/solution_spec.rb
+++ spec/2024/14/solution_spec.rb
@@ -6,18 +6,28 @@ RSpec.describe Year2024::Day14 do
   let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2024/14/input.txt')) }
   let(:example_input) do
     <<~EOF
-      change
-      me
+      p=0,4 v=3,-3
+      p=6,3 v=-1,-3
+      p=10,3 v=-1,2
+      p=2,0 v=2,-1
+      p=0,0 v=1,3
+      p=3,0 v=-2,-2
+      p=7,6 v=-1,-3
+      p=3,0 v=-1,-2
+      p=9,3 v=2,3
+      p=7,3 v=-1,2
+      p=2,4 v=2,-3
+      p=9,5 v=-3,-3
     EOF
   end
 
   describe 'part 1' do
-    it 'returns nil for the example input' do
-      expect(described_class.part_1(example_input)).to eq(nil)
+    it 'returns 12 for the example input' do
+      expect(described_class.part_1(example_input)).to eq(12)
     end
 
-    it 'returns nil for my input' do
-      expect(described_class.part_1(input)).to eq(nil)
+    it 'returns 233709840 for my input' do
+      expect(described_class.part_1(input)).to eq(233_709_840)
     end
   end
 

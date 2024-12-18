1705558369136722ecfa3a1b9e03beecb4d373b7
Tomas229 <tomas229cortez@gmail.com>
2024-12-17 23:02:14 -0300

    feat(2024-17): Agrega respuesta parte 1

--- challenges/2024/17/solution.rb
+++ challenges/2024/17/solution.rb
@@ -7,21 +7,74 @@ module Year2024
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
 
     def part_1
-      nil
+      instructions = data[4][9..].split(',')
+      res = []
+      while @pointer < instructions.size
+        cal = calculate_operation(instructions[@pointer], instructions[@pointer + 1])
+        res += [cal] unless cal.empty?
+        @pointer += 2
+      end
+      res.join(',')
     end
 
     def part_2
       nil
     end
 
-    # Processes each line of the input file and stores the result in the dataset
-    # def process_input(line)
-    #   line.map(&:to_i)
-    # end
+    def setup_data
+      @memory = { A: data[0][/\d+/].to_i, B: data[1][/\d+/].to_i, C: data[2][/\d+/].to_i }
+      @pointer = 0
+    end
 
-    # Processes the dataset as a whole
-    # def process_dataset(set)
-    #   set
-    # end
+    private
+
+    def calculate_operation(opcode, operant)
+      case opcode
+      when '0' then op_div(:A, operant)
+      when '1' then op_xor(operant)
+      when '2' then op_modulo(operant)
+      when '3' then op_jump(operant)
+      when '4' then op_xor
+      when '5' then op_print(operant)
+      else op_div(:C, operant) end
+    end
+
+    def combo_operand(operand)
+      case operand
+      when '4' then @memory[:A]
+      when '5' then @memory[:B]
+      when '6' then @memory[:C]
+      else
+        operand.to_i
+      end
+    end
+
+    def literal_operand(operand)
+      operand.to_i
+    end
+
+    def op_div(sym, operant)
+      @memory[sym] = (@memory[:A] / (2**combo_operand(operant))).truncate
+      ''
+    end
+
+    def op_xor(operant=nil)
+      @memory[:B] = @memory[:B] ^ (operant.nil? ? @memory[:C] : literal_operand(operant))
+      ''
+    end
+
+    def op_modulo(operant)
+      @memory[:B] = combo_operand(operant) % 8
+      ''
+    end
+
+    def op_jump(operant)
+      @pointer = (literal_operand(operant) - 2) unless @memory[:A].zero?
+      ''
+    end
+
+    def op_print(operant)
+      (combo_operand(operant) % 8).to_s
+    end
   end
 end
--- spec/2024/17/solution_spec.rb
+++ spec/2024/17/solution_spec.rb
@@ -6,18 +6,21 @@ RSpec.describe Year2024::Day17 do
   let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2024/17/input.txt')) }
   let(:example_input) do
     <<~EOF
-      change
-      me
+      Register A: 729
+      Register B: 0
+      Register C: 0
+
+      Program: 0,1,5,4,3,0
     EOF
   end
 
   describe 'part 1' do
-    it 'returns nil for the example input' do
-      expect(described_class.part_1(example_input)).to eq(nil)
+    it 'returns 4,6,3,5,6,3,5,2,1,0 for the example input' do
+      expect(described_class.part_1(example_input)).to eq('4,6,3,5,6,3,5,2,1,0')
     end
 
-    it 'returns nil for my input' do
-      expect(described_class.part_1(input)).to eq(nil)
+    it 'returns 3,7,1,7,2,1,0,6,3 for my input' do
+      expect(described_class.part_1(input)).to eq('3,7,1,7,2,1,0,6,3')
     end
   end
 

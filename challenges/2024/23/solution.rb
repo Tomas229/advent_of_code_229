180d516cf22c848a001e035aea88c3f14d3fffda
Tomas229 <tomas229cortez@gmail.com>
2024-12-24 17:06:50 -0300

    feat(2024-23): Agrega respuesta parte 1

--- challenges/2024/23/solution.rb
+++ challenges/2024/23/solution.rb
@@ -7,21 +7,34 @@ module Year2024
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
 
     def part_1
-      nil
+      @count = 0
+      @connections.each do |first_pc, first_pc_connections|
+        first_pc_connections.each do |second_pc|
+          connections = obtain_t_connections(first_pc, second_pc, first_pc_connections)
+          @count += connections&.size || 0
+        end
+      end
+      @count
     end
 
     def part_2
       nil
     end
 
-    # Processes each line of the input file and stores the result in the dataset
-    # def process_input(line)
-    #   line.map(&:to_i)
-    # end
+    def setup_data
+      @connections = {}
+      data.each do |d|
+        connection = d.split('-').sort
+        @connections[connection[0]] = (@connections[connection[0]] || []) + [connection[1]]
+      end
+    end
 
-    # Processes the dataset as a whole
-    # def process_dataset(set)
-    #   set
-    # end
+    def obtain_t_connections(first_pc, second_pc, first_pc_connections)
+      both_pc_connections = @connections[second_pc]&.public_send(:&, first_pc_connections)
+      unless first_pc.start_with?('t') || second_pc.start_with?('t')
+        both_pc_connections&.select! { |pc| pc.start_with? 't' }
+      end
+      both_pc_connections
+    end
   end
 end
--- spec/2024/23/solution_spec.rb
+++ spec/2024/23/solution_spec.rb
@@ -6,18 +6,48 @@ RSpec.describe Year2024::Day23 do
   let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2024/23/input.txt')) }
   let(:example_input) do
     <<~EOF
-      change
-      me
+      kh-tc
+      qp-kh
+      de-cg
+      ka-co
+      yn-aq
+      qp-ub
+      cg-tb
+      vc-aq
+      tb-ka
+      wh-tc
+      yn-cg
+      kh-ub
+      ta-co
+      de-co
+      tc-td
+      tb-wq
+      wh-td
+      ta-ka
+      td-qp
+      aq-cg
+      wq-ub
+      ub-vc
+      de-ta
+      wq-aq
+      wq-vc
+      wh-yn
+      ka-de
+      kh-ta
+      co-tc
+      wh-qp
+      tb-vc
+      td-yn
     EOF
   end
 
   describe 'part 1' do
-    it 'returns nil for the example input' do
-      expect(described_class.part_1(example_input)).to eq(nil)
+    it 'returns 7 for the example input' do
+      expect(described_class.part_1(example_input)).to eq(7)
     end
 
-    it 'returns nil for my input' do
-      expect(described_class.part_1(input)).to eq(nil)
+    it 'returns 1485 for my input' do
+      expect(described_class.part_1(input)).to eq(1485)
     end
   end
 

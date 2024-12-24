df30c0c0eadf5e0803b81f6ff3dafd8cb1cec9b6
Tomas229 <tomas229cortez@gmail.com>
2024-12-24 13:44:46 -0300

    feat(2024-24): Agrega respuesta parte 1

--- challenges/2024/24/solution.rb
+++ challenges/2024/24/solution.rb
@@ -7,21 +7,66 @@ module Year2024
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
 
     def part_1
-      nil
+      @instructions.each do |instruction|
+        operand_1 = instruction[0]
+        operand_2 = instruction[2]
+
+        if @memory[operand_1].nil? || @memory[operand_2].nil?
+          @instructions << instruction.dup
+        else
+          update_memory(operand_1, operand_2, instruction[1], instruction[3])
+        end
+      end
+      memory_to_number(@memory)
     end
 
     def part_2
       nil
     end
 
-    # Processes each line of the input file and stores the result in the dataset
-    # def process_input(line)
-    #   line.map(&:to_i)
-    # end
+    def setup_data
+      i = data.find_index('')
+      setup_memory(data[0...i])
+      setup_instructions(data[i + 1..])
+    end
 
-    # Processes the dataset as a whole
-    # def process_dataset(set)
-    #   set
-    # end
+    private
+
+    def setup_memory(memory_data)
+      @memory = {}
+      memory_data.each do |line|
+        wire = line.split(':')
+        @memory[wire[0]] = !wire[1].to_i.zero?
+      end
+    end
+
+    def setup_instructions(instructions_data)
+      @instructions = instructions_data.map do |line|
+        instructions = line.split('->')
+        inputs = instructions[0].split
+        [inputs[0], inputs[1], inputs[2], instructions[1].strip]
+      end
+    end
+
+    def memory_to_number(memory_data)
+      keys = memory_data.keys.select { |key| key.start_with? 'z' }.sort.reverse
+      num = ''
+      keys.each do |key|
+        num += memory_data[key] ? '1' : '0'
+      end
+      num.to_i(2)
+    end
+
+    def update_memory(operand_1, operand_2, operation, output)
+      res = case operation
+            when 'AND'
+              @memory[operand_1] && @memory[operand_2]
+            when 'OR'
+              @memory[operand_1] || @memory[operand_2]
+            else
+              @memory[operand_1] ^ @memory[operand_2]
+            end
+      @memory[output] = res
+    end
   end
 end
--- spec/2024/24/solution_spec.rb
+++ spec/2024/24/solution_spec.rb
@@ -6,18 +6,63 @@ RSpec.describe Year2024::Day24 do
   let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2024/24/input.txt')) }
   let(:example_input) do
     <<~EOF
-      change
-      me
+      x00: 1
+      x01: 0
+      x02: 1
+      x03: 1
+      x04: 0
+      y00: 1
+      y01: 1
+      y02: 1
+      y03: 1
+      y04: 1
+
+      ntg XOR fgs -> mjb
+      y02 OR x01 -> tnw
+      kwq OR kpj -> z05
+      x00 OR x03 -> fst
+      tgd XOR rvg -> z01
+      vdt OR tnw -> bfw
+      bfw AND frj -> z10
+      ffh OR nrd -> bqk
+      y00 AND y03 -> djm
+      y03 OR y00 -> psh
+      bqk OR frj -> z08
+      tnw OR fst -> frj
+      gnj AND tgd -> z11
+      bfw XOR mjb -> z00
+      x03 OR x00 -> vdt
+      gnj AND wpb -> z02
+      x04 AND y00 -> kjc
+      djm OR pbm -> qhw
+      nrd AND vdt -> hwm
+      kjc AND fst -> rvg
+      y04 OR y02 -> fgs
+      y01 AND x02 -> pbm
+      ntg OR kjc -> kwq
+      psh XOR fgs -> tgd
+      qhw XOR tgd -> z09
+      pbm OR djm -> kpj
+      x03 XOR y03 -> ffh
+      x00 XOR y04 -> ntg
+      bfw OR bqk -> z06
+      nrd XOR fgs -> wpb
+      frj XOR qhw -> z04
+      bqk OR frj -> z07
+      y03 OR x01 -> nrd
+      hwm AND bqk -> z03
+      tgd XOR rvg -> z12
+      tnw OR pbm -> gnj
     EOF
   end
 
   describe 'part 1' do
-    it 'returns nil for the example input' do
-      expect(described_class.part_1(example_input)).to eq(nil)
+    it 'returns 2024 for the example input' do
+      expect(described_class.part_1(example_input)).to eq(2024)
     end
 
-    it 'returns nil for my input' do
-      expect(described_class.part_1(input)).to eq(nil)
+    it 'returns 48806532300520 for my input' do
+      expect(described_class.part_1(input)).to eq(48_806_532_300_520)
     end
   end
 

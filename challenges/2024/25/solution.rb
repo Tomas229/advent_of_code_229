ce8d824b7e2f8d7c72beb390137a1523c072dbbd
Tomas229 <tomas229cortez@gmail.com>
2024-12-25 12:46:02 -0300

    feat(2024-25): Agrega respuesta parte 1

--- challenges/2024/25/solution.rb
+++ challenges/2024/25/solution.rb
@@ -7,21 +7,58 @@ module Year2024
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
 
     def part_1
-      nil
+      count = 0
+      @keys.each_value do |key|
+        @locks.each_value do |lock|
+          count += compare_fit(key, lock)
+        end
+      end
+      count
     end
 
     def part_2
       nil
     end
 
-    # Processes each line of the input file and stores the result in the dataset
-    # def process_input(line)
-    #   line.map(&:to_i)
-    # end
+    def setup_data
+      @keys = {}
+      @locks = {}
+      i = 0
+      j = 7
+      while j < data.size + 1
+        data[i].start_with?('.') ? setup_key(i, j) : setup_lock(i, j)
+        i += 8
+        j += 8
+      end
+    end
 
-    # Processes the dataset as a whole
-    # def process_dataset(set)
-    #   set
-    # end
+    private
+
+    def setup_key(lower_index, upper_index)
+      key = [0] * 5
+      data[lower_index...upper_index].each_with_index do |line, row|
+        get_string_indexes(line, '#').each { |col| key[col] = [key[col], 6 - row].max }
+      end
+      @keys[lower_index] = key
+    end
+
+    def setup_lock(lower_index, upper_index)
+      lock = [5] * 5
+      data[lower_index...upper_index].each_with_index do |line, row|
+        get_string_indexes(line, '.').each { |col| lock[col] = [lock[col], row - 1].min }
+      end
+      @locks[lower_index] = lock
+    end
+
+    def get_string_indexes(string, char)
+      (0...string.length).find_all { |i| string[i, 1] == char }
+    end
+
+    def compare_fit(key, lock)
+      5.times do |i|
+        return 0 if key[i] + lock[i] > 5
+      end
+      1
+    end
   end
 end
--- spec/2024/25/solution_spec.rb
+++ spec/2024/25/solution_spec.rb
@@ -6,18 +6,56 @@ RSpec.describe Year2024::Day25 do
   let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2024/25/input.txt')) }
   let(:example_input) do
     <<~EOF
-      change
-      me
+      #####
+      .####
+      .####
+      .####
+      .#.#.
+      .#...
+      .....
+
+      #####
+      ##.##
+      .#.##
+      ...##
+      ...#.
+      ...#.
+      .....
+
+      .....
+      #....
+      #....
+      #...#
+      #.#.#
+      #.###
+      #####
+
+      .....
+      .....
+      #.#..
+      ###..
+      ###.#
+      ###.#
+      #####
+
+      .....
+      .....
+      .....
+      #....
+      #.#..
+      #.#.#
+      #####
+
     EOF
   end
 
   describe 'part 1' do
-    it 'returns nil for the example input' do
-      expect(described_class.part_1(example_input)).to eq(nil)
+    it 'returns 3 for the example input' do
+      expect(described_class.part_1(example_input)).to eq(3)
     end
 
-    it 'returns nil for my input' do
-      expect(described_class.part_1(input)).to eq(nil)
+    it 'returns 3155 for my input' do
+      expect(described_class.part_1(input)).to eq(3155)
     end
   end
 
